abf62f389d94c39cd687de02b68738a53614cd68
cantuc40 <cantuc40@gator.uhd.edu>
2022-01-14 12:19:31 -0600

    Added Register Component, Replaced Switch Keyword with Routes

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-import './App.css';
+import {Route, Routes} from 'react-router-dom';
+//import './App.css';
 
 import Display from './pages/display.component';
 import Header from './components/header.component';
 import AddFilm from './pages/add-film.component';
 import DeleteFilm from './pages/delete-film.component';
 import EditFilm from './pages/edit-film.component';
+import Register from './pages/register.component'
 
 
 function App() {
   return (
     <div>
       <Header/>
-      <Switch>
+      <Routes>
         <Route path='/' exact component={Display}/>
+        <Route path='/register' component={Register}/>
         <Route path='/add' component={AddFilm}/>
-        <Route path= '/modify/:id' component={EditFilm}/>
-        <Route path='/delete/:id' component={DeleteFilm}/>
-      </Switch>
+        <Route path= '/modify' component={EditFilm}/>
+        <Route path='/delete' component={DeleteFilm}/>
+      </Routes>
 
     </div>
   );
