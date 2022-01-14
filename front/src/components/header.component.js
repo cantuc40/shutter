34d7bb0f2b3369286a17ecafb147351c23e1e1ad
cantuc40 <cantuc40@gator.uhd.edu>
2022-01-14 12:20:20 -0600

    added nav template

--- front/src/components/header.component.js
+++ front/src/components/header.component.js
@@ -11,17 +11,30 @@ export default class Header extends Component{
     render() {
         return(
             <div>
-                <ul>
-                    <li>
-                        <Link to="/add">Add Film</Link>
-                    </li>
-                    <li>
-                        <Link to="/modify">Modify Film</Link>
-                    </li>
-                    <li>
-                        <Link to="/delete">Delete Film</Link>
-                    </li>
-                </ul>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <Link to="/">Shutter</Link>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarColor03">
+                            <ul class="navbar-nav me-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="#">Films
+                                        <span class="visually-hidden">(current)</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Cameras</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Movie Films</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
             </div>
         );
     }
