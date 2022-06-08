eca31acd5b5f7aabe517adaacde706fe747c598a
cantuc40 <cantuc40@gator.uhd.edu>
2022-06-08 14:51:26 -0500

    refactored code

--- cinematography/views.py
+++ cinematography/views.py
@@ -1,3 +1,4 @@
+
 from django.http import HttpResponse
 from .models import *
 from django.shortcuts import render, get_object_or_404
--- shutter/urls.py
+++ shutter/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
+from . import views
 
 urlpatterns = [
     path('photography/', include('photography.urls')),
     path('cinematography/', include('cinematography.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index)
 ]
